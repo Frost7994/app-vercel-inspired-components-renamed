442ef9f51d49ded1611bdd16769be7b4c7b0568e
Frost7994 <tommybyrne9@gmail.com>
2023-04-23 12:08:51 +0100

    added toggle

--- src/pages/test/component.js
+++ src/pages/test/component.js
@@ -1,8 +1,17 @@
+import Toggle from "@/components/ui/toggle"
+import { useState } from "react"
 
 const Component = () => {
 
+    const [enabled, setEnabled] = useState(false)
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+            <Toggle size='sm' enabled={enabled} setEnabled={setEnabled} />
+            <Toggle enabled={enabled} setEnabled={setEnabled} />
+            <Toggle size='lg' enabled={enabled} setEnabled={setEnabled} />
+            <Toggle disabled enabled={enabled} setEnabled={setEnabled} />
+            <Toggle variant='primary' enabled={enabled} setEnabled={setEnabled} />
+            <Toggle variant='secondary' enabled={enabled} setEnabled={setEnabled} />
 
         </div>
     )
