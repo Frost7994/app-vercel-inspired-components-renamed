bea17cca1745bb43156347b67f88a67d7571e74c
Frost7994 <tommybyrne9@gmail.com>
2023-04-24 15:45:06 +0100

    added checkbox

--- src/pages/test/component.js
+++ src/pages/test/component.js
@@ -1,11 +1,40 @@
-import TableDemonstration from "@/components/mdx/demonstration/tableDemonstration"
+import Button from "@/components/ui/button"
+import Checkbox from "@/components/ui/checkbox"
+import { useForm, Controller } from "react-hook-form"
 
-const Test = () => {
+const Component = () => {
+  // form destructure
+  const { register, handleSubmit, errors, control } = useForm({
+    defaultValues: {
+      terms: false
+    }
+  })
+
+  // fn submit
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div>
-      <TableDemonstration />
+    <div className="flex items-center justify-center h-full">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex items-center gap-2">
+        <Controller
+          control={control}
+          name='terms'
+          render={({ field: { value, onChange } }) =>
+            <Checkbox
+              checked={value}
+              setChecked={onChange}
+            />
+          } />
+
+        <p>Agree to terms and conditions.</p>
+        <Button size='sm' type='submit'>Submit</Button>
+      </form>
     </div>
   )
 }
 
-export default Test
+export default Component
